919bfbf2c2d6543c55ef721b6ef34c17dc2231d8
LuisandLan <ghg00rus300@gmail.com>
2023-01-09 19:15:16 +0700

    add LazyColumn

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -20,9 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
-import com.codelab.basiclayouts.ui.theme.shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -105,15 +105,15 @@ fun FavoriteCollectionCard(
             Image(
                 painter = painterResource(drawable),
                 contentDescription = null,
-                contentScale =ContentScale.Crop,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(56.dp)
             )
             Text(
                 text = stringResource(text),
                 style = MaterialTheme.typography.h3,
-                modifier = Modifier.padding(horizontal =16.dp )
-                )
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
         }
     }
 }
@@ -123,7 +123,15 @@ fun FavoriteCollectionCard(
 fun AlignYourBodyRow(
     modifier: Modifier = Modifier,
 ) {
-    // Implement composable here
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
 }
 
 // Step: Favorite collections grid - LazyGrid
@@ -157,7 +165,12 @@ private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
 // Step: MySoothe App - Scaffold
 @Composable
 fun MySootheApp() {
-    // Implement composable here
+    SearchBar(Modifier.padding(8.dp))
+    AlignYourBodyElement(
+        text = R.string.ab1_inversions,
+        drawable = R.drawable.ab1_inversions,
+        modifier = Modifier.padding(8.dp)
+    )
 }
 
 private val alignYourBodyData = listOf(
